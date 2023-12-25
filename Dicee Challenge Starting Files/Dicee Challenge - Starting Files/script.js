781f9d6b1c8345502e366c3b42fa5cb9ae51a85d
artistic-whale <artistic_whale_x@proton.me>
2023-12-25 16:48:20 +0100

    Made a dice game :game_die:

---
+++ Dicee Challenge Starting Files/Dicee Challenge - Starting Files/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+
+let randomNumber1 = Math.floor(Math.random() * 6 + 1);
+let randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+let randomDiceImage = "images/dice" + randomNumber1 + ".png";
+let randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+console.log(randomNumber1);
+
+let imageOne = document.querySelector(".img1").setAttribute("src", randomDiceImage);
+let imageTwo  = document.querySelector(".img2").setAttribute("src", randomDiceImage2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!"
+} else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}

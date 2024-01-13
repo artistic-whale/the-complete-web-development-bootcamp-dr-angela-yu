f218375d8660fae1be53a6012f7cfc22be5fb8c3
artistic-whale <artistic_whale_x@proton.me>
2024-01-13 19:27:48 +0100

    139. Higher Order Function Challenge Solution

---
+++ Calculator/script.js
@@ -0,0 +1,23 @@
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+
+function divide(number1, number2) {
+    return number1 / number2;
+}
+
+
+function calculator(number1, number2, operator) {
+    return operator(number1, number2);
+}

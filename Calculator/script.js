7a7203cc7a48775caad56da2763f6b7a861dfd38
artistic-whale <artistic_whale_x@proton.me>
2024-01-13 22:46:18 +0100

    Get value from button click on console

--- Calculator/script.js
+++ Calculator/script.js
@@ -21,3 +21,24 @@ function divide(number1, number2) {
 function calculator(number1, number2, operator) {
     return operator(number1, number2);
 }
+
+
+// if (button has been clicked ) {
+//     then show clicked button on display
+// }
+
+let getAllKeys          = document.querySelectorAll("button");
+let calculatorDisplay   = document.getElementsByClassName("calculator__display");
+let numberKey0          = document.querySelector("#number0");
+
+
+for (let i = 0; i < getAllKeys.length; i++) {
+    getAllKeys[i].addEventListener("click", function () {
+        calculatorDisplay.textContent = getAllKeys[i];
+        console.log([i]);
+    });
+}
+
+// if (document.querySelector("#number0").addEventListener("click")) {
+//     document.querySelector("#calculator__display").innerHTML = "0";
+// }

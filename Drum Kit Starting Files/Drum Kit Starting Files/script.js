dc9b07ab9a102f0583f9c54c25b63c86265828f7
artistic-whale <artistic_whale_x@proton.me>
2024-01-14 16:13:15 +0100

    139. How to Play Sounds on a Webiste :ballot_box_with_check: :musical_note:

--- Drum Kit Starting Files/Drum Kit Starting Files/script.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/script.js
@@ -9,5 +9,11 @@ for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         // alert('I got clicked');
         audio.play();
+
+        if (this.innerHTML === 'w') {
+            audio.play("sounds/tom-1.mp3");
+            document.getElementsByClassName("w")[0].style.color = "white";
+        }
+
     })
 }

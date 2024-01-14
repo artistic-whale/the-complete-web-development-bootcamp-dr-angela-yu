5a5657c7c2742bc88e140877a9853654b4ad9a56
artistic-whale <artistic_whale_x@proton.me>
2024-01-14 15:04:18 +0100

    Added background-images + sounds to drum kit :musical_note:

--- Drum Kit Starting Files/Drum Kit Starting Files/script.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/script.js
@@ -1,7 +1,13 @@
 'use strict'
 
+
+let audio = new Audio("sounds/tom-1.mp3");
+audio.play();
+
+
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert('I got clicked');
+        // alert('I got clicked');
+        audio.play();
     })
 }

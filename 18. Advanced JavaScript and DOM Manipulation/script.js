bd8fb498e9904f0daf88a28efb7248cef44a6677
artistic-whale <artistic_whale_x@proton.me>
2024-01-17 17:10:35 +0100

    Added new module: 18. Advanced JavaScript and DOM Manipulation

---
+++ 18. Advanced JavaScript and DOM Manipulation/script.js
@@ -0,0 +1,10 @@
+'use strict'
+
+// ************ 140. A Deeper Understanding of JavaScript Objects ************
+var houseKeeper1 = {
+    name: "Cinderella",
+    age: 25,
+    yearsOfExperience: 5,
+    hotelsWorkedAt: ["Buckingham Palace Hotel", "Royal Place Hotel"],
+    cleaningRepertoire: ["bathroom", "lobby", "bedroom"]
+};

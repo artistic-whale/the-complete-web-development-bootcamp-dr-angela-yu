ed8a130d84938984ca8c877fc06ea48173be7203
artistic-whale <artistic_whale_x@proton.me>
2024-01-21 16:37:13 +0100

    Drum Kit Challenge Completed :ballot_box_with_check:

--- Drum Kit Starting Files/Drum Kit Starting Files/script.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/script.js
@@ -66,4 +66,8 @@ function makeSound(key) {
 function buttonAnimation(currentKey) {
    let activeButton = document.querySelector("." + currentKey);
    activeButton.classList.add("pressed");
+
+   setTimeout(function() {
+    activeButton.classList.remove("pressed");
+   }, 100);
 }

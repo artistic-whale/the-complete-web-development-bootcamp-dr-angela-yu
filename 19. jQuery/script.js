94c9484b4c9f9295bc5402a6609762863a836377
artistic-whale <artistic_whale_x@proton.me>
2024-01-21 23:27:56 +0100

    Some jQuery bs

--- 19. jQuery/script.js
+++ 19. jQuery/script.js
@@ -1,1 +1,21 @@
-$("h1").css("color", "white");
+// $("h1").css("color", "white");
+// not best-practice!
+
+$("h1").addClass("big-title margin-50");
+$("h1").text("Bye");
+
+$("button").click(function() {
+    $("h1").html("<b>Bye Again</b>")
+});
+
+
+// one way to use event listeners
+$(document).keydown(function(event) {
+    $("h1").text(event.key);
+});
+
+
+// another way to use event listeners
+$("h1").on("mouseover", function() {
+    $("h1").css("color", "red");
+});

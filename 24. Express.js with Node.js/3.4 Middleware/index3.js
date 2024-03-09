b80be99faf455aa2389cf53cd97155d70db0723a
design-and-codes <design-and-code@proton.me>
2024-03-09 10:18:49 +0100

    205. Custom Middleware - Logger

--- 24. Express.js with Node.js/3.4 Middleware/index3.js
+++ 24. Express.js with Node.js/3.4 Middleware/index3.js
@@ -3,7 +3,17 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+function logger(req, res, next) {
+  console.log("Request Method: ", req.method, "Request URL: ", req.url);
+  next();
+}
+
 app.use(logger);
+
+// Since we've created a separate function called logger, the line of code below is not needed now for our middleware.
+// app.use((req, res, next) => {
+//   console.log("Request method: ", req.method, "\n", "Request URL: ", req.url);
+// });
 
 app.get("/", (req, res) => {
   res.send("Hello");
